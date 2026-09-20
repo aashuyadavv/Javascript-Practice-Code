44e8a7909a0ea1558d17fa9775636839cd50883c
Aashish Yadav <aashuyadav11000@gmail.com>
2026-09-20 17:38:33 +0530

    add more javascript practice

---
+++ String Practice/index.js
@@ -0,0 +1,10 @@
+let name = "Aashu"
+let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Aashu!" when called
+
+function greetAashu() {
+    console.log(greeting + ", " + name + "!")
+}
+
+greetAashu()
