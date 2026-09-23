0415c60b9e38f0a8c8ae14f946a1deb7fac7962a
Aashish Yadav <aashuyadav11000@gmail.com>
2026-09-23 21:52:32 +0530

    continue

--- Blackjack Game/index.js
+++ Blackjack Game/index.js
@@ -1,17 +1,27 @@
 let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard + 4
+let secondCard = 4
+let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
+let message = ""
+
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?" 
+
+
+}
+
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card?")
+    message = "Do you want to draw a new card?"
 } else if (sum === 21) { 
-console.log("Wohoo!You've got Blackjack!")
+message = "Wohoo!You've got Blackjack!"
 hasBlackJack = true
 }else {
-    console.log("You're out of the game!")
+    message = "You're out of the game!"
     isAlive = false
 }
 
-console.log(isAlive)
+console.log(message)
