ffedec868e53c93c27f17a1ede0f321243c210fa
Aashish Yadav <aashuyadav11000@gmail.com>
2026-09-24 23:59:47 +0530

    start array

--- Blackjack Game/index.js
+++ Blackjack Game/index.js
@@ -33,17 +33,3 @@ function newCard() {
     console.log("Drawing a new card from the deck!")
     
 }
-
-// if (sum <= 20) {
-//     message = "Do you want to draw a new card?"
-// } else if (sum === 21) { 
-// message = "Wohoo!You've got Blackjack!"
-// hasBlackJack = true
-// }else {
-//     message = "You're out of the game!"
-//     isAlive = false
-// }
-// messageEl.textContent = message
-// }
-
-// console.log(message)
