72c070f10d9f55ec3067795a2bd599c41388f8cf
Aashish Yadav <aashuyadav11000@gmail.com>
2026-09-25 00:36:59 +0530

    use a loop to render card

--- Blackjack Game/index.js
+++ Blackjack Game/index.js
@@ -15,7 +15,10 @@ function startGame() {
 function renderGame() {
 
     cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
-    
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?" 
