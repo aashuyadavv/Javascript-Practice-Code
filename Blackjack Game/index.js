0c8b085076409afe62b14cf05df2c2a98cc1644e
Aashish Yadav <aashuyadav11000@gmail.com>
2026-09-24 23:54:52 +0530

    array intro

--- Blackjack Game/index.js
+++ Blackjack Game/index.js
@@ -1,25 +1,26 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-
 let messageEl = document.getElementById("message-el")
-
 let sumEl = document.getElementById("sum-el")
-// let sumEl = document.querySelector("body") 
-
 let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
-     
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    renderGame()
+}
+function renderGame() {
+
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?" 
     } else if (sum === 21) { 
-        message = "Wohoo!You've got Blackjack!"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
         message = "You're out of the game!"
@@ -30,6 +31,8 @@ function startGame() {
 
 function newCard() {
     console.log("Drawing a new card from the deck!")
+    
+}
 
 // if (sum <= 20) {
 //     message = "Do you want to draw a new card?"
